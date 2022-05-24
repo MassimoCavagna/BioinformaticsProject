4176a99aee9a7e84c4b5086521934997564dedd5
RicoPallazzo <43791854+Bertonc98@users.noreply.github.com>
2022-05-24 14:06:04 +0200

    test imputation and knn_imputer

--- test/test_DataPreProcessing.py
+++ test/test_DataPreProcessing.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytest
 import pandas as pd
 import sys, os
@@ -39,4 +40,27 @@ def test_max_axis_nan():
            dp.max_axis_nan(dic, keys, ['123'], 0, relative = True).equals(check_row_r) and 
            dp.max_axis_nan(dic, keys, ['123'], 1, relative = True).equals(check_col_r))
 
+def test_imputation():
+  # epig: dict, f: Callable, knn : bool = False, n_neighbors : int = 5 
+  dic = {'cellLine_promoters_123': pd.DataFrame([[1, 2, 3], [None, None, None], [4, None, 6]]), 
+          'cellLine_enhancers_123': pd.DataFrame([[1, 7, 3], [4, None, None], [5, None, 6]])
+        }
+  dic_knn = {'cellLine_promoters_123': pd.DataFrame([[1, 2, 3], [None, None, None], [4, None, 6]]), 
+             'cellLine_enhancers_123': pd.DataFrame([[1, 7, 3], [4, None, None], [5, None, 6]])
+            }
   
+  result = dp.imputation(dic, np.mean)
+  result_knn = dp.imputation(dic_knn, dp.knn_imputer, True, 2)
+  
+  check_result = {'cellLine_promoters_123': pd.DataFrame([[1, 2, 3], [2.5, 2., 4.5], [4, 2., 6]]), 
+                  'cellLine_enhancers_123': pd.DataFrame([[1, 7, 3], [4, 7., 4.5], [5, 7., 6]])
+                 }
+  check_result_knn = {'cellLine_promoters_123': pd.DataFrame([[1., 2., 3.], [2.5, 2., 4.5], [4., 2., 6.]]), 
+                      'cellLine_enhancers_123': pd.DataFrame([[1., 7., 3.], [4., 7., 4.5], [5., 7., 6.]])
+                     }
+                
+  assert (all([result[k].equals(check_result[k]) for k in dic.keys()]) and
+          all([result_knn[k].equals(check_result_knn[k]) for k in dic.keys()])
+         )
+
+
