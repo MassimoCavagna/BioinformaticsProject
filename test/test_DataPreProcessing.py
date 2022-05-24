c37b11f45d109a3e3ecd5d6501edf4bf511a224b
RicoPallazzo <43791854+Bertonc98@users.noreply.github.com>
2022-05-24 13:28:56 +0200

    Max nan test

--- test/test_DataPreProcessing.py
+++ test/test_DataPreProcessing.py
@@ -4,24 +4,39 @@ import sys, os
 
 sys.path.insert(0, os.path.abspath('/content/BioinformaticsProject'))
 
-import DataPreProcessing.max_axis_nan as dp_man
-import DataPreProcessing.imputation as dp_i
+import DataPreProcessing as dp
 
 def test_max_axis_nan():
     df_prom = pd.DataFrame([[1, 2, 3], [None, None, None], [4, None, 6]])
     df_enh =  pd.DataFrame([[1, None, 3], [4, None, None], [5, None, 6]])
     
     dic = {'cellLine_promoters_123': df_prom, 'cellLine_enhancers_123': df_enh}
-    keys = ['promoters', 'enhancers']
+    keys = list(dic.keys())
     columns = df_prom.columns
     
-    
-    check_row = pd.DataFrame([3, 2], columns = ['123'])
-    check_row.set_index(keys)
+    check_row = pd.DataFrame([3., 2.], columns = ['123'])
     check_row.index.name = 'cellLine_row'
+    check_row.index = ["promoters", "enhancers"]
     
-    check_col = pd.DataFrame([2, 3], columns = ['123'])
-    check_col.set_index(keys)
+    
+    check_col = pd.DataFrame([2., 3.], columns = ['123'])
+    check_col.index = ["promoters", "enhancers"]
     check_col.index.name = 'cellLine_col'
+
+    check_row_r = pd.DataFrame([round(1., 3), round(2/3, 3)], columns = ['123'])
+    check_row_r.index = ["promoters", "enhancers"]
+    check_row_r.index.name = 'cellLine_row'
     
-    assert dp_man(dic, keys, ['123'], 0).equals(check_row) and dp_man(dic, keys, ['123'], 1).equals(check_col)
+    
+    check_col_r = pd.DataFrame([round(2/3, 3), round(1., 3)], columns = ['123'])
+    check_col_r.index = ["promoters", "enhancers"]
+    check_col_r.index.name = 'cellLine_col'
+
+    print(check_row_r)
+    print(dp.max_axis_nan(dic, keys, ['123'], 0, relative = True))
+    assert (dp.max_axis_nan(dic, keys, ['123'], 0).equals(check_row) and 
+           dp.max_axis_nan(dic, keys, ['123'], 1).equals(check_col) and
+           dp.max_axis_nan(dic, keys, ['123'], 0, relative = True).equals(check_row_r) and 
+           dp.max_axis_nan(dic, keys, ['123'], 1, relative = True).equals(check_col_r))
+
+  
