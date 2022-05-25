9ec48247be3c363b7c791cbb2729597de7f87e92
RicoPallazzo <43791854+Bertonc98@users.noreply.github.com>
2022-05-25 22:00:13 +0200

    Update and rename test_DataDownload to test_DataDownload.py

---
+++ test/test_DataDownload.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pytest
+import pandas as pd
+import sys, os
+
+sys.path.insert(0, os.path.abspath('/content/BioinformaticsProject'))
+
+import DataPreProcessing as dp
+
+def test_download_cell_lines():
+  
+  assert False
