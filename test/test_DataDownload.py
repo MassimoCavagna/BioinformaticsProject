702e9472d4b5e18d3206e42ffac851799d726ffe
RicoPallazzo <43791854+Bertonc98@users.noreply.github.com>
2022-05-25 22:18:16 +0200

    Update test_DataDownload.py

--- test/test_DataDownload.py
+++ test/test_DataDownload.py
@@ -5,8 +5,10 @@ import sys, os
 
 sys.path.insert(0, os.path.abspath('/content/BioinformaticsProject'))
 
-import DataPreProcessing as dp
+import DataDownload as dd
 
 def test_download_cell_lines():
-  
-  assert False
+  line = ["A549"]
+  d1, d2 = dd.download_cell_lines(line)
+  print(len(d1.keys()))
+  assert len(d1.keys()) == 10
