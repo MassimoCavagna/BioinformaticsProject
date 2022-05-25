4c3ca6d6012c209291a4247c3292f8f73955758d
RicoPallazzo <43791854+Bertonc98@users.noreply.github.com>
2022-05-25 21:50:18 +0200

    Update DataDownload.py

--- DataDownload.py
+++ DataDownload.py
@@ -1,35 +1,34 @@
+from epigenomic_dataset import load_epigenomes
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+
+
 def download_cell_lines(cell_lines : list):
-  """
-    This function download and resurn the epigenomic data and labels of the requested cellular lines
-    
-    Params:
-      cell_lines: the list of cellular lines desired
-    Return:
-      Two dictionaries that contains the data and labels for each cell line and each windows size
-  """
-#   window_sizes = [2**x for x in range(6, 11)]
 
-#   original_epig = {}
-#   original_labels = {}
+  window_sizes = [2**x for x in range(6, 11)]
 
-#   for cell_line in cell_lines:
-#     print(cell_line)
-#     for window_size in tqdm(window_sizes):
-#       promoters_epigenomes, promoters_labels = load_epigenomes(
-#                                                 cell_line = cell_line,
-#                                                 dataset = "fantom",
-#                                                 region = "promoters",
-#                                                 window_size = window_size
-#                                                 )
-#       enhancers_epigenomes, enhancers_labels = load_epigenomes(
-#                                                 cell_line = cell_line,
-#                                                 dataset = "fantom",
-#                                                 region = "enhancers",
-#                                                 window_size = window_size
-#                                                 )
-#       original_epig[cell_line + '_promoters_' + str(window_size)] = promoters_epigenomes
-#       original_labels[cell_line + '_promoters_' + str(window_size)] = promoters_labels
-#       original_epig[cell_line + '_enhancers_' + str(window_size)] = enhancers_epigenomes
-#       original_labels[cell_line + '_enhancers_' + str(window_size)] = enhancers_labels
-#   return original_epig, original_labels
-return 1,1
+  original_epig = {}
+  original_labels = {}
+
+  for cell_line in cell_lines:
+    print("---",cell_line)
+    for window_size in tqdm(window_sizes):
+      promoters_epigenomes, promoters_labels = load_epigenomes(
+                                                cell_line = cell_line,
+                                                dataset = "fantom",
+                                                region = "promoters",
+                                                window_size = window_size
+                                                )
+      enhancers_epigenomes, enhancers_labels = load_epigenomes(
+                                                cell_line = cell_line,
+                                                dataset = "fantom",
+                                                region = "enhancers",
+                                                window_size = window_size
+                                                )
+      original_epig[cell_line + '_promoters_' + str(window_size)] = promoters_epigenomes
+      original_labels[cell_line + '_promoters_' + str(window_size)] = promoters_labels
+      original_epig[cell_line + '_enhancers_' + str(window_size)] = enhancers_epigenomes
+      original_labels[cell_line + '_enhancers_' + str(window_size)] = enhancers_labels
+  return original_epig, original_labels
+
